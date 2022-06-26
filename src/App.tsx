b3bf0e084128d99d29e130fa09ac4970a97c9984
flamarionfagundes <flamarion.fagundes@ighthouseit.com.br>
2022-06-25 22:04:41 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { gql, useQuery } from "@apollo/client"
+import { useEffect } from "react"
+import { client } from './lib/apolo'
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`
+
+interface Lesson {
+  id: string,
+  title: string
+}
+
+function App() {
+  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
+  console.log("🚀 ~ file: App.tsx ~ line 16 ~ App ~ data", data)
+
+  return (
+    <ul>
+      {data?.lessons.map(lesson => {
+        return <li key={lesson.id}>{lesson.title}</li>
+      })}
+    </ul>
+  )
+}
+
+export default App
